8f4b6d287ed5b0c3a06ee20201a896f2e3b7bc6e
staroul <34680777+staroul@users.noreply.github.com>
2018-05-13 00:47:53 +0800

    修改根URL，对各个小类对应URL进行抓取

--- request_data.py
+++ request_data.py
@@ -2,18 +2,40 @@ import requests
 import re
 from bs4 import BeautifulSoup
 
-# 获取唯品会网站
-root = requests.get('https://www.vip.com/')
-# print(root.text)
+# 获取唯品会商品页面内容
+root = requests.get('https://category.vip.com/')
+
+# 将网页文本写入txt中使用编辑器打开方便查看如何过滤数据
+# filename = 'sample.html'
+# with open(filename, 'w', encoding='utf-8') as file:
+#     file.write(root.text)
 
 # 生成BeautifulSoup对象
 # 使用lxml第三方解析器
 soup = BeautifulSoup(root.text, "lxml")
 
-filename = 'sample.txt'
+# 获取json数据所需tree_id
+tree_id = re.search(r'"tree_id":(\d+)', soup.text)
 
-with open(filename, 'w', encoding='utf-8') as file:
-    file.write(root.text)
+# 获取json数据所需cid
+#  中文编码之 .encode('utf-8').decode('unicode_escape')
+c_id = re.findall(r'"cate_id":"(\d+)"', soup.text)
+# if c_id:
+#     for cid in c_id:
+#         # 获取json数据url
+#         json_url = 'https://category.vip.com/ajax/getTreeList.php?cid={}&tree_id={}'.format(cid[1], tree_id.group(2))
+#         print(json_url)
 
-# main_kind = soup.find_all("a")
-# print(main_kind)
+# 测试获取json中的各个小类的url
+format_url = 'https://category.vip.com/ajax/getTreeList.php?cid={}&tree_id={}'.format(c_id[0], tree_id.group(1))
+req_url = requests.get(format_url)
+
+# filename = 'sample_req.json'
+# with open(filename, 'w', encoding='utf-8') as file:
+#     file.write(req_url.text)
+
+soup_url = BeautifulSoup(req_url.text, "lxml")
+
+cate = re.findall(r'"cate_id":"(\d+)","cate_type":"(3)","cate_name":"([\\u\w+/]+)",.*?"url":"(.*?)"', soup_url.text)
+if cate:
+    print(cate)
